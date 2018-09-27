be8c97743c1ae4311204373a0a819274060ae404
Nigma Nugraha <nigmanugraha@gmail.com>
2018-09-27 21:46:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nigma/biodata/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.nigma.biodata;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View v){
+        Uri uri = Uri.parse("geo:-6.9826264,100.4070121?q=" + Uri.encode("UDINUS"));
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        it.setPackage("com.google.android.apps.maps");
+        startActivity(it);
+
+    }
+
+    public void onCall(View v){
+        String phone = "082328252646";
+        Uri uri = Uri.parse("tel:"+phone);
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void onEmail(View v){
+        Uri uri = Uri.parse("mailto:nigmanugraha@gmaill.com");
+        Intent it = new Intent(Intent.ACTION_SENDTO,uri);
+        startActivity(it);
+    }
+}
